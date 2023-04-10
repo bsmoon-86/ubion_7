c3b4c84a40a07399d20f624aaf07f151ec0ef895
bsmoon-86 <ekflafg@naver.com>
2023-04-10 21:41:17 +0900

    230410_update

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 # 라이브러리 로드 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify
+import pandas as pd
 
 # Flask 클래스 생성
 # __name__ : 현재 파일의 이름
@@ -12,11 +13,36 @@ app = Flask(__name__)
 # localhost:3000/ 요청 시 아래의 함수를 실행
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
 @app.route("/second")
 def second():
-    return "Second Page"
+    return render_template("second.html")
+
+# post형태의 api 생성
+@app.route("/login", methods=['post'])
+def login():
+    # 유저가 보낸 데이터를 변수에 대입
+    # _id부분은 html에 input 안에 name이라는 속성의 값
+    id = request.form['_id']
+    password = request.form['_pass']
+    print(id, password)
+    if (id == "test") and (password == "1234"):
+        return render_template("main.html", _id = id)
+    else:
+        return redirect("/")
+    
+@app.route("/corona")
+def corona():
+    # get형태에서 데이터를 받아서 변수에 대입 
+    servicekey = request.args['servicekey']
+    print(servicekey)
+    if servicekey == "aaa":
+        df = pd.read_csv("../csv/corona.csv")
+        json_data = df.to_json()
+        return jsonify(json_data)
+    else:
+        return "servicekey error"
 
 
 
