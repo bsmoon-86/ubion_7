ffee3e75a30d8f04b35de7649e8bd3268f6533d5
bsmoon-86 <ekflafg@naver.com>
2023-05-23 01:01:55 +0900

    sample_server_update

--- sample_server/main.py
+++ sample_server/main.py
@@ -15,19 +15,21 @@ def index():
     # 일일 확진자 파생변수
     # x축에는 등록일시
     # y축에는 일일확진자
-    df = pd.read_csv("../csv/corona.csv")
+    df = pd.read_csv("./csv/corona.csv")
     df.columns = ["인덱스", "등록일시", "사망자", "확진자", "게시글번호", 
                     "기준일", "기준시간", "수정일시", "누적의심자", 
                     "누적확진률"]
     df.sort_values("등록일시", inplace=True)
     df["일일확진자"] = df["확진자"].diff().fillna(0)
-    df_2 = df.tail(100)
+    df["일일사망자"] = df["사망자"].diff().fillna(0)
+    df_2 = df.tail(50)
     _x = df_2["등록일시"].tolist()
     _y = df_2["일일확진자"].tolist()
     data = df_2.to_dict()
     cnt = len(df_2)
     columns = df_2.columns  # 데이터형 list
     label =  '일일확진자'
+    c_cnt = len(columns)
     # print(data)
     # _x = [1,2,3,4,5]
     # _y = [40, 20, 10, 40, 100]
@@ -37,41 +39,16 @@ def index():
                             cnt = cnt, 
                             data = data, 
                             columns = columns, 
+                            c_cnt = c_cnt,
                             name = label)
 
-@app.route("/index2")
-def index2():
-    df = pd.read_csv("../csv/corona.csv")
-    df.columns = ["인덱스", "등록일시", "사망자", "확진자", "게시글번호", 
-                    "기준일", "기준시간", "수정일시", "누적의심자", 
-                    "누적확진률"]
-    df.sort_values("등록일시", inplace=True)
-    df["일일확진자"] = df["확진자"].diff().fillna(0)
-    df["일일사망자"] = df["사망자"].diff().fillna(0)
-    df_2 = df.tail(50)
-    _x = df_2["등록일시"].tolist()
-    _y = df_2["일일사망자"].tolist()
-    data = df_2.to_dict()
-    cnt = len(df_2)
-    columns = df_2.columns  # 데이터형 list
-    c_cnt = len(columns)
-    # print(data)
-    # _x = [1,2,3,4,5]
-    # _y = [40, 20, 10, 40, 100]
-    return render_template("index2.html", 
-                            x_pos=_x, 
-                            y_pos=_y, 
-                            cnt = cnt, 
-                            data = data, 
-                            c_cnt = c_cnt, 
-                            columns = columns)
 
 @app.route("/invest")
 def test():
     file_name = request.args['file']
     invest_type = request.args['type']
     print(file_name, invest_type)
-    df = pd.read_csv(f"../csv/{file_name}.csv", index_col='Date')
+    df = pd.read_csv(f"./csv/{file_name}.csv", index_col='Date')
     invest_class = invest.Invest(df)
     if invest_type == 'buyandhold':
         result = invest_class.buyandhold()
@@ -87,7 +64,7 @@ def test():
     label = '누적수익율'
     print("column : ", columns)
     print("DATA :",  result.head(5).to_dict(orient='records'))
-    return render_template('index.html', 
+    return render_template('invest.html', 
                            x_pos = _x, 
                            y_pos = _y, 
                            cnt = cnt, 
